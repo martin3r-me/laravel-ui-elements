8e4b70c3163bcf893128e79eb87f99fcfcdf88fe
Martin Erren <martin3r@Mac.fritz.box>
2025-07-09 13:23:49 +0200

    init

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,6 +2,6 @@
 @php
     $cfg = config('ui-elements.button')[$variant];
 @endphp
-<button {{ \$attributes->merge(['class' => "px-4 py-2 rounded {$cfg['bg']} {$cfg['text']}\"]) }}>
+<button {{ \$attributes->merge(['class' => "px-4 py-2 rounded {$cfg['bg']} {$cfg['text']}"]) }}>
     {{ $slot }}
 </button>
