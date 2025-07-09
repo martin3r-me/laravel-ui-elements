412eb700f7d4bf65d2c03707a40cac10726eac20
Martin Erren <martin3r@Mac.fritz.box>
2025-07-09 14:05:36 +0200

    init

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,6 +2,8 @@
 @php
     $cfg = config('ui-elements.button')[$variant];
 @endphp
-<button {{ $attributes->merge(['class' => "px-4 py-2 rounded {$cfg['bg']} {$cfg['text']}"]) }}>
+<button {{ $attributes->merge([
+    'class' => "cursor-pointer px-4 py-2 rounded {$cfg['bg']} {$cfg['text']}",
+]) }}>
     {{ $slot }}
 </button>
