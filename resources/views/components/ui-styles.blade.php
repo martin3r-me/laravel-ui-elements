5c6296cea3888ae47e5680199fec03d2cb179615
Martin Erren <martin3r@Mac.fritz.box>
2025-07-09 15:25:55 +0200

    init

--- resources/views/components/ui-styles.blade.php
+++ resources/views/components/ui-styles.blade.php
@@ -1,10 +1,21 @@
 <style>
 :root {
-    --ui-primary:   {{ config('ui-elements.colors.primary') }};
-    --ui-secondary: {{ config('ui-elements.colors.secondary') }};
-    --ui-success:   {{ config('ui-elements.colors.success') }};
-    --ui-danger:    {{ config('ui-elements.colors.danger') }};
-    --ui-warning:   {{ config('ui-elements.colors.warning') }};
-    --ui-info:      {{ config('ui-elements.colors.info') }};
+    --ui-primary:        {{ config('ui-elements.colors.primary.bg') }};
+    --ui-on-primary:     {{ config('ui-elements.colors.primary.on') }};
+
+    --ui-secondary:      {{ config('ui-elements.colors.secondary.bg') }};
+    --ui-on-secondary:   {{ config('ui-elements.colors.secondary.on') }};
+
+    --ui-success:        {{ config('ui-elements.colors.success.bg') }};
+    --ui-on-success:     {{ config('ui-elements.colors.success.on') }};
+
+    --ui-danger:         {{ config('ui-elements.colors.danger.bg') }};
+    --ui-on-danger:      {{ config('ui-elements.colors.danger.on') }};
+
+    --ui-warning:        {{ config('ui-elements.colors.warning.bg') }};
+    --ui-on-warning:     {{ config('ui-elements.colors.warning.on') }};
+
+    --ui-info:           {{ config('ui-elements.colors.info.bg') }};
+    --ui-on-info:        {{ config('ui-elements.colors.info.on') }};
 }
 </style>
